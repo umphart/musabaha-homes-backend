47f83a566a838a214393173f78ab22f18b7cac2d
Umar Musa Halliru <umusa5801@gmail.com>
2025-10-14 17:31:39 +0100

    'userl'

--- routes/subscriptions.js
+++ routes/subscriptions.js
@@ -538,7 +538,7 @@ router.put("/:id", async (req, res) => {
 
     res.json({
       success: true,
-      message: "Subscription updated successfully",
+      message: "Subscription updated successfull..y",
       data: result.rows[0]
     });
   } catch (error) {
